203cbc21445c412a51231931ca54347882439450
manoel <figueiredoufam@gmail.com>
2019-08-20 13:35:29 +0100

    Backend pronto com reset password

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/noderest');
+mongoose.connect('mongodb://localhost/noderest', { useFindAndModify: false, useNewUrlParser: true, useCreateIndex: true });
+//Original mongoose.connect('mongodb://localhost/noderest');
+
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
---
+++ backend/src/modules/mailer.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+const nodemailer = require('nodemailer');
+const hbs = require('nodemailer-express-handlebars');
+
+const { host, port, user, pass } = require('../config/mailer.json');
+
+const transport = nodemailer.createTransport({
+  host,
+  port,
+  auth: { user, pass },
+});
+
+// transport.use('compile', hbs({
+//   viewEngine: 'handlebars',
+//   viewPath: path.resolve('./src/resources/mail/'),
+//   extName: '.html',
+// }));
+
+
+const handlebarOptions = {
+  viewEngine: {
+    extName: '.html',
+    partialsDir: path.resolve('./src/resources/mail/'),
+    layoutsDir: path.resolve('./src/resources/mail/'),
+    defaultLayout: '',
+  },
+  viewPath: path.resolve('./src/resources/mail/'),
+  extName: '.html',
+}
+transport.use('compile', hbs(handlebarOptions));
+module.exports =  transport;
